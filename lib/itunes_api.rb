326ac6af8c341bf454d55b8cf111f80fabb9ca83
Richard Livsey <richard@livsey.org>
2010-07-16 04:38:45 +0100

    Make the specs for ItunesAPI#find_album pass, so it now returns the JSON result of an album if it finds one.

--- lib/itunes_api.rb
+++ lib/itunes_api.rb
@@ -1,15 +1,35 @@
 # Helper to search the iTunes API
-# 
-# For now this just returns the parsed JSON as a hash, it's conceivable that 
+#
+# For now this just returns the parsed JSON as a hash, it's conceivable that
 # this could transform them into nicer objects if needed
-# 
-# For this exercise we're just concerned with looking up individual albums, 
+#
+# For this exercise we're just concerned with looking up individual albums,
 # so that's all that's implemented
-# 
+#
 # See the docs for details on the full API
 # http://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html
 
+require 'net/http'
+require 'uri'
+
 module ItunesAPI
+
+  API_URI = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsLookup"
+
   def self.find_album(id)
+    uri      = URI.parse("#{API_URI}?id=#{id}")
+    response = Net::HTTP.get_response(uri)
+
+    begin
+      results = JSON.parse(response.body)
+    rescue JSON::ParserError
+     # silently fail for now, in future we will want to handle this appropriately
+     return
+    end
+
+    return if results["resultCount"] == 0
+
+    # return the first album we find
+    results["results"].find{|result| result["collectionType"] == "Album" }
   end
 end
--- spec/lib/itunes_api_spec.rb
+++ spec/lib/itunes_api_spec.rb
@@ -5,39 +5,40 @@ ITUNES_API_URI = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/w
 describe ItunesAPI do
   describe "#find_album" do
     it "should call the iTunes API with the correct ID" do
+      stub_request(:get, "#{ITUNES_API_URI}?id=123456")
       ItunesAPI.find_album(123456)
       WebMock.should have_requested(:get, "#{ITUNES_API_URI}?id=123456")
     end
 
     it "should return nil when the API returns no results" do
-      stub_request(:get, ITUNES_API_URI).to_return(:body => fixture_file("nothing.json"))
+      stub_request(:get, "#{ITUNES_API_URI}?id=123456").to_return(:body => fixture_file("nothing.json"))
       ItunesAPI.find_album(123456).should be_nil
     end
 
     it "should return nil when the API returns results, but no albums" do
-      stub_request(:get, ITUNES_API_URI).to_return(:body => fixture_file("no_albums.json"))
+      stub_request(:get, "#{ITUNES_API_URI}?id=123456").to_return(:body => fixture_file("no_albums.json"))
       ItunesAPI.find_album(123456).should be_nil
     end
 
     it "should return the first album in the list when the API returns multiple albums" do
-      stub_request(:get, ITUNES_API_URI).to_return(:body => fixture_file("albums.json"))
+      stub_request(:get, "#{ITUNES_API_URI}?id=123456").to_return(:body => fixture_file("albums.json"))
       album = ItunesAPI.find_album(123456)
       album.should_not be_nil
-      album[:collectionId].should == 66314629
+      album["collectionId"].should == 66314629
     end
 
     it "should return the album when the API returns one album" do
-      stub_request(:get, ITUNES_API_URI).to_return(:body => fixture_file("album.json"))
+      stub_request(:get, "#{ITUNES_API_URI}?id=123456").to_return(:body => fixture_file("album.json"))
       album = ItunesAPI.find_album(123456)
       album.should_not be_nil
-      album[:collectionId].should == 66314629
+      album["collectionId"].should == 66314629
     end
 
     it "should return the first album in the list when the API returns an artist with albums" do
-      stub_request(:get, ITUNES_API_URI).to_return(:body => fixture_file("artist_and_albums.json"))
+      stub_request(:get, "#{ITUNES_API_URI}?id=123456").to_return(:body => fixture_file("artist_and_albums.json"))
       album = ItunesAPI.find_album(123456)
       album.should_not be_nil
-      album[:collectionId].should == 373809929
+      album["collectionId"].should == 373809929
     end
   end
 end
